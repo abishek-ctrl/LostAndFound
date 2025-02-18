da1da6a01bb27ae26278881ea318942a6c8057ef
Abishek <amabishek02@email.com>
2025-02-18 11:25:50 +0530

    book management backend

--- server/models/Todo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const TodoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  completed: { type: Boolean, default: false }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Todo', TodoSchema);
--- server/server.js
+++ server/server.js
@@ -1,33 +1,18 @@
 require('dotenv').config();
-const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const connectDB= require('./databases/db');
+const bookRoutes = require('./routes/book-routes');
 
-const app = express();
-
-// Middleware
-app.use(express.json()); // Parse JSON bodies
-app.use(cors());         // Enable CORS
-
-// Database connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error('MongoDB connection error:', err));
-
-// Routes
-const todoRoutes = require('./routes/todo');
-app.use('/api/todos', todoRoutes);
-
-// Root route
-app.get('/', (req, res) => {
-  res.send('Welcome to the To-Do List API!');
-});
-
-// Start server
+express=require("express");
+app=express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+connectDB();
+
+app.use(express.json());
+
+app.use('/api/book',bookRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
